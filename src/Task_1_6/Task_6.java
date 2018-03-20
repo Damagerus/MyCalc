aeb9355877dfd5e6a2815270946897303bb72462
Damagerus <Damagerus13@gmail.com>
2018-03-20 23:32:28 +0200

    My Calculator

---
+++ src/Task_1_6/Task_6.java
@@ -0,0 +1,23 @@
+package Task_1_6;
+
+public class Task_6 {
+    public static void main(String[] args) {
+        //////////////////                 a)                /////////////////////////////////////////////
+        System.out.println();
+        System.out.println("a)");
+        System.out.println();
+        System.out.println();
+        //////////////////                 b)                /////////////////////////////////////////////
+        System.out.println();
+        System.out.println("b)");
+        System.out.println();
+        System.out.println();
+    }
+
+
+
+
+
+
+
+}
